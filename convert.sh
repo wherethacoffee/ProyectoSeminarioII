fc35552d313da6137522c4980c66bc2005f1643f
wherethacoffee <whr.thacoff23@gmail.com>
2023-11-12 20:27:07 -0600

    se han actualizado algunos titulos

--- convert.sh
+++ convert.sh
@@ -2,4 +2,5 @@ pandoc ArticuloJorgeCedillo.md \
     --filter pandoc-citeproc \
     --bibliography=index.bib \
     --csl advanced-structural-and-chemical-imaging.csl \
+    -V lang=es-ES \
     -o ArticuloJorgeCedillo.pdf
